7c85006d99933ac3e1a98ddf4b25af1481eb95cd
Aleksandr Yermolaev <ermolaev.jp@gmail.com>
2019-03-25 23:31:07 +0200

    js next

---
+++ 1.propertiesAndMethods.js
@@ -0,0 +1,10 @@
+// var n = -1;
+// console.log( -1 / 0 ); 
+
+// var a = 1;
+// var b = 0;
+// var result = a/b;
+// console.log(result); 
+
+var n = 9999999999999999;
+console.log( 9999999999999999 );   // ростущее число) Причина та же – потеря точности.
---
+++ 2.propertiesAndMethods.js
@@ -0,0 +1,5 @@
+// var a = +prompt("Введите первое число", "");
+// var b = +prompt("Введите второе число", "");
+
+// console.log( a + b );
+console.log( 6.35.toFixed(16) ); 
