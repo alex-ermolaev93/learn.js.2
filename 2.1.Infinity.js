e815de14f81ddab3e8f4d2b6beecf0acee69f62a
Aleksandr Yermolaev <ermolaev.jp@gmail.com>
2019-03-27 16:57:19 +0200

    1

--- 2.1.Infinity.js
+++ 2.1.Infinity.js
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<script>
+
+
 
     console.log( 3.14/0 );
 
@@ -9,19 +9,18 @@
 
     console.log( 0/0 );
 
-</script>
 
-<script>
+
+
 
     var n = 0 / 0;
 
     console.log( isNaN(n) ); 
     console.log( isNaN("6.626") );
 
-</script>
 
-<script>
+
+
     
-    var n = 0/0;
+    var n = 0 / 0;
     if ( n !== n ) console.log( 'n = NaN!')
-</script>
